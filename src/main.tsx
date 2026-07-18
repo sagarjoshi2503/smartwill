30227696bd05c42e78d4baf0c7c5b6e8428fdf18
sagar2503 <sagar2503@users.noreply.github.com>
2026-07-18 10:50:04 +0530

    import { Analytics } from "@vercel/analytics/react"

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import SmartWill from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <SmartWill />
+    <Analytics />
   </React.StrictMode>
 );
